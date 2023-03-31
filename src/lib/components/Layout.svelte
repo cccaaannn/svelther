ec59105cba91dc41e741a405cf94eb3334e8152a
cccaaannn <can.kurt.aa@gmail.com>
2023-04-01 00:23:15 +0300

    components

---
+++ src/lib/components/Layout.svelte
@@ -0,0 +1,14 @@
+<main>
+	<div class="container">
+		<slot></slot>
+	</div>
+</main>
+
+<style>
+	.container {
+		height: 90vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
