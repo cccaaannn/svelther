cd6561c2fb8cc6fae36de3a4b7ec96360445ab93
cccaaannn <can.kurt.aa@gmail.com>
2023-04-02 04:52:03 +0300

    app name changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/weather-svelte-app/",
+  base: "/svelther/",
   plugins: [svelte()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
